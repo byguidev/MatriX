bb642eab27abbb7e8f031e85c306adbe3fe0a591
Guilherme <guilhermelacerda146@gmail.com>
2026-06-03 19:41:52 -0300

    ajusta estilo dos formulários de autenticação do usuário

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import SidebarDesktop from "./components/.common/sidebar-desktop";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import ManageStudents from "./pages/student/manage-students";
 import ManageCourses from "./pages/course/manage-courses";
 import ManageClasses from "./pages/class/manage-classes";
@@ -8,35 +8,41 @@ import StudentProfile from "./pages/student/student-profile";
 import CourseProfile from "./pages/course/course-profile";
 import ClassProfile from "./pages/class/class-profile";
 import HomePage from "./pages/home/home-page";
-import UserLogin from "./components/user/user-login";
-import UserSignUp from "./components/user/user-sign-up";
+import UserLogin from "./pages/user/user-login";
+import UserSignUp from "./pages/user/user-sign-up";
 
 import "./styles/global.css";
 
-// organiza o layout base e define as rotas principais do painel
 function App() {
   return (
-    // mantem sidebar fixa e area de conteudo navegavel
+    <BrowserRouter>
+      <AppShell />
+    </BrowserRouter>
+  );
+}
+
+function AppShell() {
+  const location = useLocation();
+  const isAuthPage = location.pathname === "/login" || location.pathname === "/sign-up";
+
+  return (
     <div className="d-flex flex-grow-1 h-100" style={{ minWidth: 0 }}>
-      <BrowserRouter>
-        <SidebarDesktop />
-        <div className="routes-container content-wrapper">
-          <Routes>
-            {/* separa os fluxos de alunos, cursos e turmas */}
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path="/login" element={<UserLogin />} />
-            <Route path="/sign-up" element={<UserSignUp />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/manage-students" element={<ManageStudents />} />
-            <Route path="/manage-students/:id" element={<StudentProfile />} />
-            <Route path="/manage-courses" element={<ManageCourses />} />
-            <Route path="/manage-courses/:id" element={<CourseProfile />} />
-            <Route path="/manage-classes" element={<ManageClasses />} />
-            <Route path="/manage-classes/:id" element={<ClassProfile />} />
-            <Route path="/manage-invoices" element={<ManageInvoices />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {!isAuthPage && <SidebarDesktop />}
+      <div className={`routes-container ${isAuthPage ? '' : 'content-wrapper'}`}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/login" element={<UserLogin />} />
+          <Route path="/sign-up" element={<UserSignUp />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/manage-students" element={<ManageStudents />} />
+          <Route path="/manage-students/:id" element={<StudentProfile />} />
+          <Route path="/manage-courses" element={<ManageCourses />} />
+          <Route path="/manage-courses/:id" element={<CourseProfile />} />
+          <Route path="/manage-classes" element={<ManageClasses />} />
+          <Route path="/manage-classes/:id" element={<ClassProfile />} />
+          <Route path="/manage-invoices" element={<ManageInvoices />} />
+        </Routes>
+      </div>
     </div>
   );
 }
